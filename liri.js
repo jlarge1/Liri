02822f7f9e3d9eb0a275762c4836a58a8ac54429
jlarge1 <largejonathan@gmail.com>
2018-07-01 14:06:24 -0400

    complete and working

--- liri.js
+++ liri.js
@@ -6,7 +6,6 @@ var keys = require("./keys");
 var request = require("request");
 var fs = require("fs");
 
-
 //twitter and spoitfy keys
 var spotify = new Spotify(keys.spotify);
 var client = new Twitter(keys.twitter);
@@ -29,17 +28,68 @@ function myTweets() {
 };
 
 function spotifySong(song) {
-    spotify.search({ type: 'track', query: song }, function (err, data, response) {
-        if (err) {
-            console.log('Error occurred: ' + err);
-        }
-        console.log(data.tracks.items[0]);
-    });
+    if (song) {
+        spotify.search({ type: 'track', query: song }, function (err, data, response) {
+            if (err) {
+                console.log('Error occurred: ' + err);
+            }
+            // console.log(data.tracks.items[0]);
+            console.log(data.tracks.items[0].artists[0].name);
+            console.log(data.tracks.items[0].name);
+            console.log(data.tracks.items[0].external_urls.spotify);
+            console.log(data.tracks.items[0].album.name);
+        });
+    }
+    else {
+        spotify.search({ type: 'track', query: 'the sign - ace of base' }, function (err, data, response) {
+            if (err) {
+                console.log('Error occurred: ' + err);
+            }
+            console.log(data.tracks.items[0].artists[0].name);
+            console.log(data.tracks.items[0].name);
+            console.log(data.tracks.items[0].external_urls.spotify);
+            console.log(data.tracks.items[0].album.name);
+        });
+    }
 }
 
-function 
+function movieThis(movie) {
+    if (movie) {
+        var queryUrl = "http://www.omdbapi.com/?t=" + movie + "&y=&plot=short&apikey=trilogy";
 
-if (userCommand == "do-what-it-says" )  {
+        request(queryUrl, function (error, response, body) {
+            if (!error && response.statusCode === 200) {
+                var obj = JSON.parse(body);
+                console.log("Title: " + obj.Title);
+                console.log("Year: " + obj.Year);
+                console.log("IMDB Rating: " + obj.imdbRating);
+                console.log("Rotten Tomatoes Rating: " + obj.Ratings[1].Value);
+                console.log("Country: " + obj.Country);
+                console.log("Language: " + obj.Language);
+                console.log("Plot: " + obj.Plot);
+                console.log("Actors: " + obj.Actors);
+            }
+        });
+    } else {
+        var queryUrl = "http://www.omdbapi.com/?t=Mr.+Nobody&y=&plot=short&apikey=trilogy";
+
+        request(queryUrl, function (error, response, body) {
+            if (!error && response.statusCode === 200) {
+                var obj = JSON.parse(body);
+                console.log("Title: " + obj.Title);
+                console.log("Year: " + obj.Year);
+                console.log("IMDB Rating: " + obj.imdbRating);
+                console.log("Rotten Tomatoes Rating: " + obj.Ratings[1].Value);
+                console.log("Country: " + obj.Country);
+                console.log("Language: " + obj.Language);
+                console.log("Plot: " + obj.Plot);
+                console.log("Actors: " + obj.Actors);
+            }
+        });
+    }
+}
+
+function doWhatSays() {
     fs.readFile("random.txt", "utf8", function (error, data) {
 
         if (error) {
@@ -50,70 +100,33 @@ if (userCommand == "do-what-it-says" )  {
 
         userCommand = dataArr[0];
         userInput = dataArr[1];
-        console.log(userCommand);
+        console.log(data);
+        main(userCommand, userInput);
     });
-    
-} 
+}
 
-switch (userCommand) {    
-    case "my-tweets":
-        myTweets();
-        break;
-    case "spotify-this-song":
-        spotifySong(userInput);
-        break;
-    case "movie-this":
+function main(command, input) {
+    switch (command) {
+        case "my-tweets":
+            myTweets();
+            break;
+        case "spotify-this-song":
+            spotifySong(input);
+            break;
+        case "movie-this":
+            movieThis(input);
+            break;
+        case "do-what-it-says":
+            doWhatSays();
+            break;
+        default:
+            console.log("liri accepts the following commands: my-tweets, spotify-this-song, movie-this, do-what-it-says")
+    }
 
-        if (userInput) {
-            var queryUrl = "http://www.omdbapi.com/?t=" + userInput + "&y=&plot=short&apikey=trilogy";
-
-            request(queryUrl, function (error, response, body) {
-                if (!error && response.statusCode === 200) {
-                    var obj = JSON.parse(body);
-                    console.log("Title: " + obj.Title);
-                    console.log("Year: " + obj.Year);
-                    console.log("IMDB Rating: " + obj.imdbRating);
-                    console.log("Rotten Tomatoes Rating: " + obj.Ratings[1].Value);
-                    console.log("Country: " + obj.Country);
-                    console.log("Language: " + obj.Language);
-                    console.log("Plot: " + obj.Plot);
-                    console.log("Actors: " + obj.Actors);
-                }
-            });
-        } else {
-            var queryUrl = "http://www.omdbapi.com/?t=Mr.+Nobody&y=&plot=short&apikey=trilogy";
-
-            request(queryUrl, function (error, response, body) {
-                if (!error && response.statusCode === 200) {
-                    var obj = JSON.parse(body);
-                    console.log("Title: " + obj.Title);
-                    console.log("Year: " + obj.Year);
-                    console.log("IMDB Rating: " + obj.imdbRating);
-                    console.log("Rotten Tomatoes Rating: " + obj.Ratings[1].Value);
-                    console.log("Country: " + obj.Country);
-                    console.log("Language: " + obj.Language);
-                    console.log("Plot: " + obj.Plot);
-                    console.log("Actors: " + obj.Actors);
-                }
-            });
-        }
-
-        break;
-    default:
-        console.log("liri accepts the following commands: my-tweets, spotify-this-song, movie-this, do-what-it-says")
 }
 
 
 
-
-
-// spotify.search({ type: 'track', query: 'All the Small Things' }, function (err, data) {
-//     if (err) {
-//         return console.log('Error occurred: ' + err);
-//     }
-
-//     console.log(data);
-// });
-
-
+//main 
+main(userCommand, userInput);
 
